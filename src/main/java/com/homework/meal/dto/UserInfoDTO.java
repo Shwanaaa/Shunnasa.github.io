7d7a40124c0b84375b149dec76b796c9bd83c8d4
Jiaman05 <jiaman.chen05@gmail.com>
2025-07-04 21:35:05 +0800

    fix[]: 个人信息完善参数修改

--- src/main/java/com/homework/meal/dto/UserInfoDTO.java
+++ src/main/java/com/homework/meal/dto/UserInfoDTO.java
@@ -27,8 +27,5 @@ public class UserInfoDTO {
     @Size(max = 11, min = 11, message = "手机号格式错误！")
     private String phone;
 
-    @NotBlank(message = "邮箱不能为空")
-    @Email(message = "邮箱格式不正确")
-    private String email;
 
 }
